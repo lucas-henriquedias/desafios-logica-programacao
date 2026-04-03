64bde89da26a8d7c10d6488ff3dbf72387a2bb83
Lucas Henrique Dias <166299365+lucas-henriquedias@users.noreply.github.com>
2026-04-03 16:20:04 +0000

    mexando no X

--- logica-desenho-x/main.py
+++ logica-desenho-x/main.py
@@ -1,59 +1,71 @@
+import sys
+
+def repeat_question():
+    while True:
+        try:
+            escolha = int(input(f"\n{"-"*40}\nGostaria de fazer de novo:\n1 - Sim\n2 - Não\n\n >> "))
+            if (escolha == 1):
+                break
+
+            elif (escolha == 2):
+                print("\nEncerrando programa...")
+                sys.exit()
+
+            else:
+                print("Selecione uma opção válida.")
+                
+
+        except ValueError:
+            print("\nSelecione uma opção válida")
 
 
 def desenhando_x (valor):
     metade = 0
-    cont = valor
-                                #revisar variáveis, criar variaveis para espaçamento de dentro e de fora
-    i = 0
-    j = 0
+    i = 0           #contador para a parte de cima do X.
+    j = 0           #cont para a parte de baixo.
 
-    if (valor % 2 == 0):            
-        metade = (valor // 2) - 1
+    """ eu preciso pensar em alguma forma de primeiro, imprimir o X, dar uma série de espaços
+    na qual pode ter algum calculo que faça valor -2, enquanto dar espaços antes o segunda
+    linha em diante. """
 
-        for i in range(int(metade)):          #melhor tentar usar while! 
-            print(f"X{' '*valor}X")
-            valor += 1
 
-        print(f"{' '*cont}X")
+    metade = (valor // 2)
 
-        for j in range(int(metade)):
-            print(f"X{' '*valor}X")
-            valor -= 1
-   
-    else:
-        
-        metade = valor // 2
+    for i in range(metade):
+        print(f"{' '*i}")
 
-        for i in range(metade):
+        for j in range(metade):
             print(f"X{' '*valor}X")
             valor -= 1
-        
-        print(f"X{' '*cont}")
+
+    for i in range(metade, 0):
+        print(f"{' '*i}")
 
         for j in range(metade):
             print(f"X{' '*valor}X")
             valor += 1
 
 
+#################################################
 
 while True:
     try:
         print("\n=== Desenhando um X ===")
         print("-"*30)
         valor = int(input("Digite o tamanho que o X terá: "))
+        print(" ")
 
         if valor <= 2:
             print("valor deve ser de 3 em diante.")
             continue
 
         else:
+            #Chamandos a função resposnsável pelos calculos
             desenhando_x(valor)
-            escolha = int(input(f"\n{"-"*40}\nGostaria de fazer de novo:\n1 - Sim\n2 - Não\n\n >> "))
-            if (escolha == 1):
-                continue
-            else:
-                break
+            repeat_question()
 
     except ValueError:
         print("\nO valor deve ser um num inteiro.")
+
+
 
